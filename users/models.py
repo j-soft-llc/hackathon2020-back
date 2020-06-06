136a9cd75268e4fc19ec0364ec95f564ecbc21ea
Andrey Bodukhin <andreywatsup@gmail.com>
2020-06-06 23:47:17 +0300

    Add vote methods

--- users/models.py
+++ users/models.py
@@ -7,39 +7,56 @@ from geo.models import Geo
 
 class User(AbstractUser):
 
-    avatar_link = models.URLField('Аватар', max_length=200, blank=True, null=True)
-    vk_id = models.CharField('Id Вк', max_length=50, blank=True, null=True)
-    instagram_id = models.CharField('Id Instagram', max_length=50, blank=True, null=True)
+    avatar_link = models.URLField('Аватар', max_length=200,
+                                  blank=True, null=True)
+    vk_id = models.CharField('Id Вк', max_length=50,
+                             blank=True, null=True)
+    instagram_id = models.CharField('Id Instagram', max_length=50,
+                                    blank=True, null=True)
     first_name = models.CharField('Имя', max_length=50, blank=True, null=True)
-    second_name = models.CharField('Фамилия', max_length=50, blank=True, null=True)
-    middle_name = models.CharField('Отчество', max_length=50, blank=True, null=True)
+    second_name = models.CharField('Фамилия', max_length=50,
+                                   blank=True, null=True)
+    middle_name = models.CharField('Отчество', max_length=50,
+                                   blank=True, null=True)
     age = models.PositiveSmallIntegerField('Возраст', blank=True, null=True)
-    profession = models.CharField('Проффессия', max_length=200, blank=True, null=True)
-    address = models.ForeignKey(Geo, related_name='address_geo', on_delete=models.SET_NULL, blank=True, null=True)
-    district = models.ForeignKey(Geo, related_name='district_geo', on_delete=models.SET_NULL, blank=True, null=True)
+    profession = models.CharField('Проффессия', max_length=200,
+                                  blank=True, null=True)
+    address = models.ForeignKey(Geo, related_name='address_geo',
+                                on_delete=models.SET_NULL, blank=True,
+                                null=True)
+    district = models.ForeignKey(Geo, related_name='district_geo',
+                                 on_delete=models.SET_NULL, blank=True,
+                                 null=True)
     сompetencies = models.ManyToManyField(Category, related_name='categories')
 
     @property
     def is_leader(self):
         return self.district is not None
-    # TODO: add Competencies and geo
+
+    @property
+    def vote_count(self):
+        return self.leader_vote.all().count()
 
     def get_token(self):
-        # token = Token(user=self)
-        # token.save()
         if hasattr(self, 'auth_token'):
             result_token = self.auth_token.key
         else:
             token = Token(user=self)
             token.save()
-            print()
-            print()
-            print(token)
             result_token = token.key
-        # result_token = 'test'
         return result_token
 
-# TODO: Добавить дополнительные адреса 
-# class UserAddresses(models.Model):
+    def get_vote_initiatives(self):
+        initiatives = [vote.initiative for vote in self.initiative_vote_user.all()]
+        return initiatives
 
-    
+    def get_leaders(self):
+        leaders = [vote.leader for vote in self.leader_vote_user.all()]
+        return leaders
+
+    def get_users(self):
+        users = [vote.user for vote in self.leader_vote.all()]
+        return users
+
+# TODO: Добавить дополнительные адреса
+# class UserAddresses(models.Model):
