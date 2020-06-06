f3bdd5a88b3085c8e19493829ae9feef8f618cd5
Ivan Kazionov <turnthepage41@gmail.com>
2020-06-07 02:46:29 +0300

    create initiative

--- initiative/serializers.py
+++ initiative/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-from rest_framework_recursive.fields import RecursiveField
 
-from initiative.models import Category
+from initiative.models import Category, InitiativePhoto, Initiative
 
 
 class SimpleCategorySerializer(serializers.ModelSerializer):
@@ -28,3 +27,40 @@ class FullCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'children']
+
+
+class InitiativePhotoCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = InitiativePhoto
+        fields = '__all__'
+
+
+class InitiativeCreateSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        photos_fields = kwargs.pop('photos', None)
+        super().__init__(*args, **kwargs)
+        if photos_fields:
+            # да file field хотя должен быть image field, ругается на png, не хочу искать настройки
+            # TODO переделать потом на ImageField
+            field_update_dict = {field: serializers.FileField(required=False, write_only=True) for field in
+                                 photos_fields}
+            self.fields.update(**field_update_dict)
+
+    def create(self, validated_data):
+        from django.core.files.uploadedfile import InMemoryUploadedFile
+        validated_data_copy = validated_data.copy()
+        validated_files = []
+        for key, value in validated_data_copy.items():
+            if isinstance(value, InMemoryUploadedFile):
+                validated_files.append(value)
+                validated_data.pop(key)
+        initiative = super().create(validated_data)
+        for file in validated_files:
+            InitiativePhoto.objects.create(initiative=initiative, photo=file)
+        return initiative
+
+    class Meta:
+        model = Initiative
+        fields = ['initiative_text', 'leader', 'category', 'initiative_type']
--- initiative/urls.py
+++ initiative/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from initiative.views import AllCategoriesListView, HeadingCategoriesListView, CategoryDetailView
+from initiative.views import AllCategoriesListView, HeadingCategoriesListView, CategoryDetailView, CreateInitiative
 
 urlpatterns = [
     path('categories/', HeadingCategoriesListView.as_view()),
     path('all_categories/', AllCategoriesListView.as_view()),
     path('category/<int:pk>/', CategoryDetailView.as_view()),
+
+    path('initiative/create/', CreateInitiative.as_view()),
 
     # path('geo/', GeoAPIView.as_view()),
 ]
