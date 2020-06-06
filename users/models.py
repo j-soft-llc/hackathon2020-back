a25b13124e46f7996f463238877273451a905b6a
Andrey Bodukhin <andreywatsup@gmail.com>
2020-06-06 15:29:56 +0300

    Add users app

---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from rest_framework.authtoken.models import Token
+
+
+class User(AbstractUser):
+    
+    # TODO: add geo_id
+
+    def get_token(self):
+        print()
+        print(type(self))
+        # token = Token(user=self)
+        # token.save()
+        # if hasattr(self, 'auth_token'):
+        #     result_token = self.auth_token.key
+        # else:
+        #     token = Token(user=self)
+        #     token.save()
+        #     print()
+        #     print()
+        #     print(token)
+        #     result_token = token.key
+        result_token = 'test'
+        return result_token
