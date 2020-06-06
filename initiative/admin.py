bdf15b967732c35bcfb87950776671f3eaea424e
Andrey Bodukhin <andreywatsup@gmail.com>
2020-06-06 21:33:06 +0300

    Add initiative admin panel

--- initiative/admin.py
+++ initiative/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from initiative.models import Category
+from initiative.models import Category, Initiative, InitiativePhoto
+
+
+class InitiativePhotoInline(admin.TabularInline):
+    model = InitiativePhoto
+
+
+class InitiativeAdmin(admin.ModelAdmin):
+    inlines = [InitiativePhotoInline]
+
 
 admin.site.register(Category)
+admin.site.register(Initiative, InitiativeAdmin)
