d725c53beb046bd0a1387a7a5bbb9c2b466a53b0
Andrey Bodukhin <andreywatsup@gmail.com>
2020-06-06 18:29:35 +0300

    Add geo to users parser

--- users/management/commands/parseusers.py
+++ users/management/commands/parseusers.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from users.models import User
+from initiative.models import Geo
 
 class Command(BaseCommand):
     help = 'Парсинг пользователей из users.csv'
@@ -12,10 +13,17 @@ class Command(BaseCommand):
         # Получаем список имеющихся пользователей
         isset_usernames = [user.username for user in User.objects.all()]
         # Убираем имеющиеся
-        users_data = [user for user in users_data if user[1] not in isset_usernames]
+        # users_data = [user for user in users_data if user[1] not in isset_usernames]
+        # Получаем geo для оставшихся пользователей
+        addresses_id = [int(user[7]) for user in users_data if user[7] not in ['', None, 0]]
+        addresses_id += [int(user[10]) for user in users_data if user[10] not in ['', None, 0]]
+        addresses_id = list(set(addresses_id))
+        addresses = {str(geo.id): geo for geo in Geo.objects.filter(id__in=addresses_id)}
         # Создаем массив с новыми пользователями
         users = []
         for user_data in users_data:
+            address = addresses.get(user_data[7])
+            district = addresses.get(user_data[10])
             user = User(
                 username=user_data[1],
                 avatar_link=user_data[0],
@@ -24,6 +32,8 @@ class Command(BaseCommand):
                 first_name=user_data[3],
                 second_name=user_data[4],
                 age=int(user_data[6]),
+                address=address,
+                district=district
             )
             user.set_password('password')
             users.append(user)
