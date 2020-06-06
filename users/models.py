775e741a423d8c37b8c89556a3056a305041a76d
Andrey Bodukhin <andreywatsup@gmail.com>
2020-06-06 16:32:50 +0300

    Add fields

--- users/models.py
+++ users/models.py
@@ -4,8 +4,17 @@ from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
-    
-    # TODO: add geo_id
+
+    avatar_link = models.URLField('Аватар', max_length=200)
+    vk_id = models.CharField('Id Вк', max_length=50, blank=True, null=True)
+    instagram_id = models.CharField('Id Instagram', max_length=50, blank=True, null=True)
+    first_name = models.CharField('Имя', max_length=50)
+    second_name = models.CharField('Фамилия', max_length=50)
+    middle_name = models.CharField('Отчество', max_length=50, blank=True, null=True)
+    age = models.PositiveSmallIntegerField('Возраст')
+    profession = models.CharField('Проффессия', max_length=200)
+
+    # TODO: add Competencies and geo
 
     def get_token(self):
         # token = Token(user=self)
