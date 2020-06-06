079c80a4bf02826ae8b5b51071d84f2497673440
Andrey Bodukhin <andreywatsup@gmail.com>
2020-06-06 23:47:39 +0300

    add vote admin panel

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import InitiativeVote, LeaderVote
 
-# Register your models here.
+
+admin.site.register(InitiativeVote)
+admin.site.register(LeaderVote)
